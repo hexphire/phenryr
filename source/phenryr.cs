51848ecbf902062e982de21dbda8c708a83b9e17
Hexphire <hexphirebiz@gmail.com>
2021-07-07 11:11:57 -0700

    Started basic bot code.

---
+++ source/phenryr.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Net;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace phenryr.source
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
