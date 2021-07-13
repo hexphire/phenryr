07cedc4eb901352237a0f56c51dc457819fde7e7
Hexphire <hexphirebiz@gmail.com>
2021-07-12 23:28:37 -0700

    added require owner to say command

--- Modules/UtilityCommands.cs
+++ Modules/UtilityCommands.cs
@@ -24,7 +24,7 @@ namespace Phenryr.Modules
         public Task lootRollAsync() => ReplyAsync($"{rand.Next(1,100)}");
 
         [Command("say")]
-        [RoleRequiredAttribute("BotDaddy")]
+        [RequireOwner]
         public Task SayAsync([Remainder] string msg ) => ReplyAsync(msg);
         
     }
