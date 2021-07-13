ee083160762f125c83370efba4ed805c05c2121f
Hexphire <hexphirebiz@gmail.com>
2021-07-13 03:23:19 -0700

    added price info to keycheck module

--- Modules/EftKeyCheckCommands.cs
+++ Modules/EftKeyCheckCommands.cs
@@ -33,6 +33,8 @@ namespace Phenryr.Modules
         public async Task EftKeyChecker(string keyToSearch)
         {
             var eb = new EmbedBuilder();
+            var sb = new StringBuilder();
+            char curSym = '\u20BD';
             KeyList keyDB = ImportKeyData();
 
             List<EftKeyModel> keyList = keyDB.Keys;
@@ -44,14 +46,22 @@ namespace Phenryr.Modules
                 await ReplyAsync("key not found, sorry");
                 return;
             }
-   
+
+            MarketModel keyInfo = EftMarketCommands.FetchMarketInfo(targetKey.KeyName);
+               
             var lootLines = targetKey.Loot.Select(kvp => kvp.Key + ": " + string.Join(", ", kvp.Value));
 
-
+            sb.AppendLine($"Current: {keyInfo.Price}{curSym}");
+            sb.AppendLine($"24hr Average: {keyInfo.Avg24hPrice}{curSym}");
+            sb.AppendLine($"7 Day Average: {keyInfo.Avg7daysPrice}{curSym}");
+            sb.AppendLine($"Trader Price: {keyInfo.TraderPrice}{keyInfo.TraderPriceCur} from {keyInfo.TraderName}");
 
             eb.Title = targetKey.KeyName;
             eb.ThumbnailUrl = targetKey.Icon;
-            eb.AddField("Loot", string.Join(Environment.NewLine, lootLines));
+            eb.AddField("Loot:", string.Join(Environment.NewLine, lootLines));
+            eb.AddField("Price Data:", sb.ToString());
+
+            eb.WithUrl(keyInfo.WikiLink);
             
 
 
